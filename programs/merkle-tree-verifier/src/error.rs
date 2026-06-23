b89f709d41049ae020c7aa63c2fcd6fd279928ee
rahuldev7583 <rahuldev7583@gmail.com>
2026-06-23 15:30:50 +0530

    root verification added

--- programs/merkle-tree-verifier/src/error.rs
+++ programs/merkle-tree-verifier/src/error.rs
@@ -4,4 +4,6 @@ use anchor_lang::prelude::*;
 pub enum ErrorCode {
     #[msg("Custom error message")]
     CustomError,
+    #[msg("Merkle Proof doesn't match the stored root")]
+    InvalidProof,
 }
--- programs/merkle-tree-verifier/src/lib.rs
+++ programs/merkle-tree-verifier/src/lib.rs
@@ -6,6 +6,7 @@ pub mod state;
 use anchor_lang::prelude::*;
 
 pub use constants::*;
+pub use error::ErrorCode;
 pub use instructions::*;
 pub use state::*;
 
@@ -13,9 +14,66 @@ declare_id!("GSE5sgXpXQMCzTkHojpXinocCCuTJhzWMhSTQysEjsmu");
 
 #[program]
 pub mod merkle_tree_verifier {
+    use solana_program::hash::hashv;
+
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         initialize::handler(ctx)
     }
+
+    pub fn set_root(ctx: Context<SetRoot>, root: [u8; 32]) -> Result<()> {
+        ctx.accounts.state.root = root;
+        msg!("Root set {:?}", root);
+        Ok(())
+    }
+
+    pub fn verify_proof(
+        ctx: Context<VerifyProof>,
+        leaf: [u8; 32],
+        proof: Vec<[u8; 32]>,
+        index: u8,
+    ) -> Result<()> {
+        let root = ctx.accounts.state.root;
+
+        let mut current = leaf;
+
+        for (depth, sibling) in proof.iter().enumerate() {
+            let is_left = ((index >> depth) & 1) == 0;
+
+            let hash = if is_left {
+                hashv(&[&current, sibling])
+            } else {
+                hashv(&[sibling, &current])
+            };
+
+            current = hash.to_bytes();
+        }
+
+        require!(root == current, ErrorCode::InvalidProof);
+
+        msg!("Merkle Proof Verified");
+
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct SetRoot<'info> {
+    #[account(mut)]
+    admin: Signer<'info>,
+    #[account(init, payer = admin, space = 8 + 32 , seeds =[b"root"], bump)]
+    state: Account<'info, MerkleState>,
+    system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct MerkleState {
+    root: [u8; 32],
+}
+
+#[derive(Accounts)]
+pub struct VerifyProof<'info> {
+    #[account(seeds = [b"root"], bump)]
+    state: Account<'info, MerkleState>,
 }
